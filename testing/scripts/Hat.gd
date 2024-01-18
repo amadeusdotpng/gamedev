8ac607dc54e1d3814ff1f4c929796b80f1957233
Amadeus8182 <amadeus8182@gmail.com>
2024-01-17 19:09:04 -0600

    make testing project interactable

--- testing/scripts/Hat.gd
+++ testing/scripts/Hat.gd
@@ -6,5 +6,13 @@ func get_input():
 	return Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	
 func _physics_process(delta):
+	var on_cat: bool = 835 <= position.x and position.x <= 875 and 120 <= position.y and position.y <= 170 
+	var on_lain: bool = 385 <= position.x and position.x <= 425 and 195 <= position.y and position.y <= 235
+	if on_cat or on_lain:
+		get_node('../Confetti_Right').visible = true
+		get_node('..//Confetti_Left').visible = true 
+	else:
+		get_node('../Confetti_Right').visible = false
+		get_node('../Confetti_Left').visible = false
 	velocity = get_input() * SPEED
 	move_and_slide()
